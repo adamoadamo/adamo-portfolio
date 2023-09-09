d1b05bf697a3a57129b78abb29320071797aaab1
adamoadamo <adamtaylororeilly@gmail.com>
2023-09-08 23:33:31 -0400

    Updates

--- assets/scripts/alt.py
+++ assets/scripts/alt.py
@@ -1,47 +1,53 @@
 import os
-import requests
 import toml
+from azure.ai.vision import sdk
 
 AZURE_API_KEY = os.getenv("AZURE_API")
-AZURE_ENDPOINT = "https://adamtoreilly.cognitiveservices.azure.com/"
+AZURE_ENDPOINT = os.getenv("VISION_ENDPOINT")
 SITE_PATH = "content/work/"
+
+service_options = sdk.VisionServiceOptions(AZURE_ENDPOINT, AZURE_API_KEY)
 
 def get_image_description(image_path):
     try:
-        with open(image_path, 'rb') as image_data:
-            headers = {
-                'Content-Type': 'application/octet-stream',
-                'Ocp-Apim-Subscription-Key': AZURE_API_KEY,
-            }
-            params = {
-                'visualFeatures': 'Description',
-            }
-            response = requests.post(AZURE_ENDPOINT + 'vision/v3.1/analyze', headers=headers, params=params, data=image_data)
-            response_data = response.json()
-            print(response_data)  # Print the response data for debugging
-            captions = response_data.get('description', {}).get('captions', [])
-            if captions:
-                description = captions[0]['text']
+        vision_source = sdk.VisionSource(file_path=image_path)
+        analysis_options = sdk.ImageAnalysisOptions()
+        analysis_options.features = (
+            sdk.ImageAnalysisFeature.CAPTION |
+            sdk.ImageAnalysisFeature.TEXT
+        )
+        analysis_options.language = "en"
+        analysis_options.gender_neutral_caption = True
+
+        image_analyzer = sdk.ImageAnalyzer(service_options, vision_source, analysis_options)
+        result = image_analyzer.analyze()
+
+        if result.reason == sdk.ImageAnalysisResultReason.ANALYZED:
+            if result.caption is not None:
+                description = result.caption.content
                 return description
+
         print(f"No description found for {image_path}")
+
     except Exception as e:
         print(f"Failed to get description for {image_path}: {e}")
+
     return None
 
 def update_markdown_file(md_file_path, front_matter_toml, image_index, alt_text):
     try:
         with open(md_file_path, 'r', encoding='utf-8') as file:
             content = file.readlines()
-        
+
         front_matter_updated = "+++\n" + toml.dumps(front_matter_toml) + "+++\n"
-        
         line_number = content.index('+++\n') + 1  # Find the end of the front matter block
         content = content[:line_number] + [front_matter_updated] + content[line_number + 1:]
-        
+
         with open(md_file_path, 'w', encoding='utf-8') as file:
             file.writelines(content)
-        
+
         print(f"Updated alt text in {md_file_path}")
+
     except Exception as e:
         print(f"Failed to update markdown file {md_file_path}: {e}")
 
@@ -51,10 +57,10 @@ for root, dirs, files in os.walk(SITE_PATH):
             md_file_path = os.path.join(root, file_name)
             with open(md_file_path, 'r', encoding='utf-8') as file:
                 content = file.read()
-            
+
             front_matter_end = content.find('+++', 3)
             front_matter = content[3:front_matter_end]
-            
+
             data = toml.loads(front_matter)
 
             resources = data.get('resources', [])
