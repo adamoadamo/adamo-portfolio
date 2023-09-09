2fbe0204589b792c9974c349ecc8e558a1e0a9e8
adamoadamo <adamtaylororeilly@gmail.com>
2023-09-08 23:14:45 -0400

    update

--- assets/scripts/alt.py
+++ assets/scripts/alt.py
@@ -1,6 +1,6 @@
 import os
 import requests
-import toml  # Make sure to install this module
+import toml
 
 AZURE_API_KEY = os.getenv("AZURE_API")
 AZURE_ENDPOINT = "https://adamtoreilly.cognitiveservices.azure.com/"
@@ -18,45 +18,30 @@ def get_image_description(image_path):
             }
             response = requests.post(AZURE_ENDPOINT + 'vision/v3.1/analyze', headers=headers, params=params, data=image_data)
             response_data = response.json()
-            print(response_data)  # Add this line to print the response data
-            description = response_data['description']['captions'][0]['text']
-            return description
+            print(response_data)  # Print the response data for debugging
+            captions = response_data.get('description', {}).get('captions', [])
+            if captions:
+                description = captions[0]['text']
+                return description
+        print(f"No description found for {image_path}")
     except Exception as e:
         print(f"Failed to get description for {image_path}: {e}")
-        return None
+    return None
 
-
-def update_markdown_file(md_file_path, image_index, alt_text):
+def update_markdown_file(md_file_path, front_matter_toml, image_index, alt_text):
     try:
         with open(md_file_path, 'r', encoding='utf-8') as file:
             content = file.readlines()
-
-        front_matter = []
-        line_number = 0
-        recording = False
-        for line in content:
-            if line.strip() == "+++":
-                if recording:
-                    break
-                else:
-                    recording = True
-            if recording:
-                front_matter.append(line)
-            line_number += 1
-
-        front_matter_toml = toml.loads(''.join(front_matter[1:-1]))
-
-        front_matter_toml["resources"][image_index]["params"]["alt"] = alt_text
-
+        
         front_matter_updated = "+++\n" + toml.dumps(front_matter_toml) + "+++\n"
         
-        content = content[:len(front_matter)-1] + [front_matter_updated] + content[line_number:]
-
+        line_number = content.index('+++\n') + 1  # Find the end of the front matter block
+        content = content[:line_number] + [front_matter_updated] + content[line_number + 1:]
+        
         with open(md_file_path, 'w', encoding='utf-8') as file:
             file.writelines(content)
-
+        
         print(f"Updated alt text in {md_file_path}")
-
     except Exception as e:
         print(f"Failed to update markdown file {md_file_path}: {e}")
 
@@ -66,7 +51,7 @@ for root, dirs, files in os.walk(SITE_PATH):
             md_file_path = os.path.join(root, file_name)
             with open(md_file_path, 'r', encoding='utf-8') as file:
                 content = file.read()
-
+            
             front_matter_end = content.find('+++', 3)
             front_matter = content[3:front_matter_end]
             
@@ -75,8 +60,6 @@ for root, dirs, files in os.walk(SITE_PATH):
             resources = data.get('resources', [])
             for i, resource in enumerate(resources):
                 image_path = os.path.join(root, resource['src'])
-
                 alt_text = get_image_description(image_path)
-                
                 if alt_text:
-                    update_markdown_file(md_file_path, i, alt_text)
+                    update_markdown_file(md_file_path, data, i, alt_text)
